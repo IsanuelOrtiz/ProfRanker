024b97a33f2525a5d6f7f9f490f5f331d70c0d38
IsanuelOrtiz <51845110+IsanuelOrtiz@users.noreply.github.com>
2019-07-18 20:09:35 -0600

    Añadi el identity

---
+++ src/ProfRanker.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProfRanker.Web.Areas.Identity.Data;
+using ProfRanker.Web.Models;
+
+[assembly: HostingStartup(typeof(ProfRanker.Web.Areas.Identity.IdentityHostingStartup))]
+namespace ProfRanker.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DataContex>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DataContexConnection")));
+
+                services.AddDefaultIdentity<AppUser>()
+                    .AddEntityFrameworkStores<DataContex>();
+            });
+        }
+    }
+}
